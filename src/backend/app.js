da81cb7b4975eeb061e7a6fa0c8865550e5ddb08
PRIOU Antoine <antoine.priou2002@gmail.com>
2025-01-15 15:11:33 +0100

    test

--- src/backend/app.js
+++ src/backend/app.js
@@ -101,9 +101,11 @@ checkEmails();
 
 app.use(express.static(path.join(__dirname)));
 
+/*
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
+*/
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
