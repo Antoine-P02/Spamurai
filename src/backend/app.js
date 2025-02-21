4d77b8087988f8fe3ce0d7745a37f4fffe211112
PRIOU Antoine <antoine.priou2002@gmail.com>
2025-02-22 00:15:26 +0100

    removing too heavy secure cookies

--- src/backend/app.js
+++ src/backend/app.js
@@ -20,7 +20,6 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: {
-        secure: process.env.NODE_ENV === 'production', // Use secure cookies in production
         maxAge: 24 * 60 * 60 * 1000 // 24 hours
     }
 }));
